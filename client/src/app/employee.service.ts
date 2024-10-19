cfe469ef5da63051e967ac3e0a1adecb894b0a1c
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-10-19 12:43:32 +0300

    Added ASP.NET validation with data annotations

--- client/src/app/employee.service.ts
+++ client/src/app/employee.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
 import { Employee } from '../models/employee';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
--- server/server/Models/Employee.cs
+++ server/server/Models/Employee.cs
@@ -1,17 +1,25 @@
-﻿namespace server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
 {
     public class Employee
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Phone is required")]
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "Position is required")]
         public string Position { get; set; }
 
     }
